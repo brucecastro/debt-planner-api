85800b52107f4d720648fb3f8a8b96d7a3e7c09f
Bruce Castro <brucecastrog@gmail.com>
2023-09-06 09:57:39 -0500

    Initial commit

---
+++ test/smd-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SmdCdk from '../lib/smd-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/smd-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SmdCdk.SmdCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
